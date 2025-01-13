e04c42710d556977e9793ed64f280184f8def61e
Joaqquin0 <u202212648@upc.edu.pe>
2025-01-13 13:42:04 -0500

    quinto cors

--- api/products.js
+++ api/products.js
@@ -5,14 +5,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(cors({
-    origin: 'https://andeisndonacion.adensir.com'
-}));
+app.use(cors());
 app.use(bodyParser.json());
 
 // Middleware para añadir las cabeceras CORS a todas las respuestas
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://andeisndonacion.adensir.com');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
