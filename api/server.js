c49b474a836f6db73578144bc49bb1adcf3358ee
Joaqquin0 <u202212648@upc.edu.pe>
2025-01-13 12:11:30 -0500

    productjson

--- api/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const backend = require("../backend/server"); // Importa tu servidor
-
-const app = express();
-
-app.use(backend); // Usa el backend desde la carpeta /backend
-
-module.exports = app;
