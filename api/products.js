de1c276755580e8917822c6d62932b4c596eca32
Joaqquin0 <u202212648@upc.edu.pe>
2025-01-13 13:50:19 -0500

    sexto cors

--- api/products.js
+++ api/products.js
@@ -5,14 +5,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(cors({
-    origin: "http://127.0.0.1:5500/donacion.html"
-}));
+app.use(cors());
 app.use(bodyParser.json());
 
 // Middleware para añadir las cabeceras CORS a todas las respuestas
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500/donacion.html');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
@@ -41,28 +39,6 @@ app.get("/api/products", async (req, res) => {
     }
 });
 
-// Endpoint para crear una sesión de checkout
-app.post("/api/create-checkout-session", async (req, res) => {
-    const { priceId } = req.body;
 
-    try {
-        const session = await stripe.checkout.sessions.create({
-            payment_method_types: ["card"],
-            line_items: [
-                {
-                    price: priceId,
-                    quantity: 1,
-                },
-            ],
-            mode: "payment",
-            success_url: "http://example.com/success",
-            cancel_url: "http://example.com/cancel",
-        });
-
-        res.json({ url: session.url });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
 
 module.exports = app;
