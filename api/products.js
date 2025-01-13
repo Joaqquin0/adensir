6712a689fb80583dbad77634269b2a4801dd926a
Joaqquin0 <u202212648@upc.edu.pe>
2025-01-13 13:33:52 -0500

    cuarto cors

--- api/products.js
+++ api/products.js
@@ -9,6 +9,14 @@ app.use(cors({
     origin: 'https://andeisndonacion.adensir.com'
 }));
 app.use(bodyParser.json());
+
+// Middleware para añadir las cabeceras CORS a todas las respuestas
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://andeisndonacion.adensir.com');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 //
 // Endpoint para obtener productos de Stripe
 app.get("/api/products", async (req, res) => {
