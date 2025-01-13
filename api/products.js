d90a32fc1bc5b168bd2f8f02acac5fe40402eaa6
Joaqquin0 <u202212648@upc.edu.pe>
2025-01-13 15:25:28 -0500

    Middleware

--- api/products.js
+++ api/products.js
@@ -12,6 +12,16 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"]  // Cabeceras permitidas
 }));
 
+app.use((req, res, next) => {
+    if (req.method === "OPTIONS") {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        return res.status(200).end();
+    }
+    next();
+});
+
 app.use(bodyParser.json());
 
 // Endpoint para obtener productos de Stripe
