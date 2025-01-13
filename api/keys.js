40917aaacbbec3a5ecf6b1c4961efd8bd0028279
Joaqquin0 <u202212648@upc.edu.pe>
2025-01-12 20:09:10 -0500

    CORS

--- api/keys.js
+++ api/keys.js
@@ -1,5 +1,10 @@
 // /api/keys.js
 export default function handler(req, res) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Permite todos los orígenes
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Métodos permitidos
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Encabezados permitidos
+
     const { STRIPE_PUBLIC_KEY, STRIPE_SECRET_KEY } = process.env;
 
     // Verificar que las claves están presentes en las variables de entorno
