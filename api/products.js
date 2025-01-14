a61e217485edb30d45f5ed021230d5480062bb5b
Joaqquin0 <u202212648@upc.edu.pe>
2025-01-13 23:17:17 -0500

    nuevoOridyc

--- api/products.js
+++ api/products.js
@@ -1,26 +1,29 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 module.exports = async (req, res) => {
-  if (req.method === "GET") {
-    try {
-        const products = await stripe.products.list({
-            expand: ["data.default_price"],
-        });
-        const formattedProducts = products.data.map(product => ({
-            id: product.id,
-            name: product.name,
-            description: product.description,
-            image: product.images[0],
-            default_price_id: product.default_price.id,
-            price: product.default_price.unit_amount,
-            currency: product.default_price.currency,
-            recurring: product.default_price.recurring || null,
-        }));
-        return res.status(200).json(formattedProducts);
-    } catch (error) {
-        return res.status(500).json({ error: error.message });
+    if (req.method === "GET") {
+        try {
+            const products = await stripe.products.list({
+                expand: ["data.default_price"],
+            });
+
+            const formattedProducts = products.data.map(product => ({
+                id: product.id,
+                name: product.name,
+                description: product.description,
+                image: product.images[0],
+                price: product.default_price.unit_amount,
+                currency: product.default_price.currency,
+                default_price_id: product.default_price.id,
+                recurring: product.default_price.recurring || null,
+            }));
+
+            return res.status(200).json(formattedProducts);
+        } catch (error) {
+            return res.status(500).json({ error: error.message });
+        }
     }
-  }
-  res.setHeader("Allow", ["GET"]);
-  return res.status(405).end(`Method ${req.method} Not Allowed`);
+
+    res.setHeader("Allow", ["GET"]);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
 };
